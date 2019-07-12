72317605e9be1a817497d4be3fa101a5ac8bcbad
Aidan <33706566+awadin@users.noreply.github.com>
2019-07-11 22:19:21 -0500

    Education page added
    timeline functioning, transitions between pages needs work

--- Aidan Website/main.js
+++ Aidan Website/main.js
@@ -18,4 +18,34 @@ $("#mcgill").on('mouseover', () => {
   $("#m_img").attr("src", generic);
 });
 
+//For Education Page
+
+//https://webdesign.tutsplus.com/tutorials/building-a-vertical-timeline-with-css-and-a-touch-of-javascript--cms-26528
+//Review all code ^^^
+
+function isElementInViewport(el) {
+  var rect = el.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+var items = document.querySelectorAll(".timeline li");
+ 
+// code for the isElementInViewport function
+ 
+function callbackFunc() {
+  for (var i = 0; i < items.length; i++) {
+    if (isElementInViewport(items[i])) {
+      items[i].classList.add("in-view");
+    }
+  }
+}
+ 
+window.addEventListener("load", callbackFunc);
+window.addEventListener("scroll", callbackFunc);
+
 }); 
